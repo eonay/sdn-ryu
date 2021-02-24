c77fa37390e1fce314eda261e794e950e378ddf5
Eonay <eonay.web@gmail.com>
2021-02-24 13:30:57 -0300

    multi controller

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 from ssh_server_ryu import *
+from topology import topologyNetwork, setLogLevel
 
 
 
 def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+    return 0
+    #print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
 
 
 # Press the green button in the gutter to run the script.
@@ -12,4 +14,6 @@ if __name__ == '__main__':
     '''print_hi('PyCharm') '''
     configControllerMaster()
     configControllerSlave()
+    setLogLevel('info')
+    topologyNetwork()
     
